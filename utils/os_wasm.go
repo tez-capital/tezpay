7862dc96fb70989bb57ec50b713a20440f94b4de
cryi <cryi@tutanota.com>
2023-09-23 20:14:02 +0200

    compat with tinygo

--- utils/os_wasm.go
+++ utils/os_wasm.go
@@ -1,59 +1,47 @@
-//go:build !wasm
+//go:build wasm
 
 package utils
 
 import (
 	"context"
-	"os"
-	"os/signal"
-	"syscall"
+)
 
-	log "github.com/sirupsen/logrus"
+var (
+	INTERUPT_CHANNEL = make(chan struct{}, 1)
 )
 
 func CallbackOnInterrupt(ctx context.Context, cb func()) {
 	go func() {
-		c := make(chan os.Signal, 1)
-		signal.Notify(c, syscall.SIGINT, syscall.SIGTERM)
 		select {
-		case <-c:
+		case <-INTERUPT_CHANNEL:
 			cb()
 		case <-ctx.Done():
 		}
-		signal.Stop(c)
 	}()
 }
 
 type ProtectedSection struct {
 	info     string
-	ch       chan os.Signal
 	signaled bool
 }
 
-// Creates a new ProtectedSection
-// Protected section captures SIGINT and SIGTERM signals if active
 func NewProtectedSection(info string) *ProtectedSection {
 	result := &ProtectedSection{
-		ch:   make(chan os.Signal, 1),
 		info: info,
 	}
-	// goroutine to handle signals and log details
 	go func() {
 		for {
-			sig, ok := <-result.ch
+			_, ok := <-INTERUPT_CHANNEL
 			if !ok {
 				break
 			}
 			result.signaled = true
-			log.Warnf("received signal '%s', %s", sig, info)
 		}
 	}()
 
 	return result
 }
 
-// Creates a new ProtectedSection and starts it
-// Protected section captures SIGINT and SIGTERM signals
 func StartNewProtectedSection(info string) *ProtectedSection {
 	result := NewProtectedSection(info)
 	result.Start()
@@ -61,7 +49,6 @@ func StartNewProtectedSection(info string) *ProtectedSection {
 }
 
 func (p *ProtectedSection) Start() {
-	signal.Notify(p.ch, syscall.SIGINT, syscall.SIGTERM)
 }
 
 func (p *ProtectedSection) Resume() {
@@ -69,7 +56,6 @@ func (p *ProtectedSection) Resume() {
 }
 
 func (p *ProtectedSection) Stop() {
-	signal.Stop(p.ch)
 }
 
 func (p *ProtectedSection) Pause() {
@@ -78,7 +64,6 @@ func (p *ProtectedSection) Pause() {
 
 func (p *ProtectedSection) Close() {
 	p.Stop()
-	close(p.ch)
 }
 
 func (p *ProtectedSection) Signaled() bool {
