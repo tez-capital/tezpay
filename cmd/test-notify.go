de4206a1a9d041b057e8dbfc728df6adb5568889
v <cryi@tutanota.com>
2025-05-08 21:00:23 +0200

    test-notify --admin

--- cmd/test-notify.go
+++ cmd/test-notify.go
@@ -14,6 +14,7 @@ var notificationTestCmd = &cobra.Command{
 	Run: func(cmd *cobra.Command, args []string) {
 		config, _, _, _ := assertRunWithResult(loadConfigurationEnginesExtensions, EXIT_CONFIGURATION_LOAD_FAILURE).Unwrap()
 		notificator, _ := cmd.Flags().GetString(NOTIFICATOR_FLAG)
+		wantsAdmin, _ := cmd.Flags().GetBool("admin")
 		for _, notificatorConfiguration := range config.NotificationConfigurations {
 			if notificator != "" && string(notificatorConfiguration.Type) != notificator {
 				continue
@@ -26,17 +27,31 @@ var notificationTestCmd = &cobra.Command{
 				continue
 			}
 
-			err = notificator.TestNotify()
-			if err != nil {
-				slog.Warn("failed to send notification", "error", err.Error())
-				continue
+			switch wantsAdmin {
+			case true:
+				if !notificatorConfiguration.IsAdmin {
+					continue
+				}
+				err = notificator.AdminNotify("test admin notification")
+				if err != nil {
+					slog.Warn("failed to send notification", "error", err.Error())
+					continue
+				}
+			default:
+				err = notificator.TestNotify()
+				if err != nil {
+					slog.Warn("failed to send notification", "error", err.Error())
+					continue
+				}
 			}
+
 		}
 	},
 }
 
 func init() {
 	notificationTestCmd.Flags().String(NOTIFICATOR_FLAG, "", "Notify through specific notificator")
+	notificationTestCmd.Flags().Bool("admin", false, "Notify through admin notificators")
 
 	RootCmd.AddCommand(notificationTestCmd)
 }
